d631f0bd4da259aee188d478ccb461e8a76f1e61
coder121 <74172416+shakibuser@users.noreply.github.com>
2026-09-10 15:14:41 +0330

    Add files via upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from "astro/config";
+import remarkBreaks from "remark-breaks";
 
 export default defineConfig({
   site: "https://sabaseda.ir",
   output: "static",
-  trailingSlash: "always"
+  trailingSlash: "always",
+  markdown: {
+    remarkPlugins: [remarkBreaks]
+  }
 });
